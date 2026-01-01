19e773a7a43f96874fcba4d0f87ddf8c719c0e79
Chris Van Law <chris@chrisvanlaw.com>
2026-01-01 10:13:15 -0500

    FEAT: Support wildcard certificates in update-certs script
    Update certificate download logic to use wildcard certificates from S3:
    - Extract base domain from CERT_HOSTNAME (e.g., imgboard.example.com -> example.com)
    - Construct wildcard prefix as wildcard.{base_domain}
    - Download certificates from s3://{bucket}/wildcard.{domain}/ instead of exact hostname
    - Add output showing which wildcard prefix is being used
    
    Example: CERT_HOSTNAME=imgboard.example.com downloads from wildcard.example.com/

--- deploy/update-certs.sh
+++ deploy/update-certs.sh
@@ -12,10 +12,15 @@ fi
 S3_BUCKET="${S3_CERT_BUCKET:?S3_CERT_BUCKET not set}"
 CERT_HOSTNAME="${CERT_HOSTNAME:?CERT_HOSTNAME not set}"
 
-echo "Downloading certificates from S3..."
-aws s3 cp "s3://$S3_BUCKET/$CERT_HOSTNAME/cert.pem" "$CERT_DIR/cert.pem"
-aws s3 cp "s3://$S3_BUCKET/$CERT_HOSTNAME/key.pem" "$CERT_DIR/key.pem"
-aws s3 cp "s3://$S3_BUCKET/$CERT_HOSTNAME/chain.pem" "$CERT_DIR/chain.pem"
+# Extract base domain for wildcard cert lookup
+# Example: imgboard.example.com -> wildcard.example.com
+BASE_DOMAIN=$(echo "$CERT_HOSTNAME" | sed -E 's/^[^.]+\.//')
+WILDCARD_PREFIX="wildcard.$BASE_DOMAIN"
+
+echo "Downloading wildcard certificates for $BASE_DOMAIN from S3..."
+aws s3 cp "s3://$S3_BUCKET/$WILDCARD_PREFIX/cert.pem" "$CERT_DIR/cert.pem"
+aws s3 cp "s3://$S3_BUCKET/$WILDCARD_PREFIX/key.pem" "$CERT_DIR/key.pem"
+aws s3 cp "s3://$S3_BUCKET/$WILDCARD_PREFIX/chain.pem" "$CERT_DIR/chain.pem"
 
 # Set permissions
 chmod 644 "$CERT_DIR/cert.pem" "$CERT_DIR/chain.pem"
@@ -27,6 +32,7 @@ EXPIRY_EPOCH=$(date -d "$EXPIRY" +%s)
 NOW_EPOCH=$(date +%s)
 DAYS_LEFT=$(( (EXPIRY_EPOCH - NOW_EPOCH) / 86400 ))
 
+echo "Using wildcard certificate from: $WILDCARD_PREFIX"
 echo "Certificate valid until: $EXPIRY ($DAYS_LEFT days remaining)"
 if [[ $DAYS_LEFT -lt 30 ]]; then
     echo "WARNING: Certificate expires in less than 30 days"
